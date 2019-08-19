8729b36278df3752cd41820b59e9eec498aeb115
BlueQigithub <1558296235@qq.com>
2019-08-19 23:09:47 +0800

    update

--- src/components/title/TitleIndex.vue
+++ src/components/title/TitleIndex.vue
@@ -1,45 +1,44 @@
 <template>
-    <div>
-        <div class="titleIndex clearfix">
-            <i class="iconfont icon-ditu"></i>
-            <input type="text" placeholder="请输入查询内容">
-            <span class="search">查找</span>
-        </div>
+  <div>
+    <div class="titleIndex clearfix">
+      <i class="iconfont icon-ditu"></i>
+      <input type="text" placeholder="请输入查询内容" />
+      <span class="search">查找</span>
     </div>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style scoped lang="less">
-.titleIndex{
-  height: 40px;
-  line-height: 40px;
+.titleIndex {
+  height: 2.5rem;
   background: pink;
-  display: flex;
-  i{
-      font-size: 20px;
-      padding: 0 20px 0 10px;
-      float: left;
+  position: relative;
+  i {
+    font-size: 20px;
+    position: absolute;
+    left: 0.5rem;
+    top: 0.75rem;
   }
-  input{
-      border: none;
-      border-bottom: 1px solid #cccccc;
-      padding: 8px 0;
-      background: pink;
-      width:70%;
+  input {
+    border: none;
+    border-bottom: 1px solid #fff;
+    width: 70%;
+    padding: 0.5rem 0;
+    background: pink;
+    margin: 0.25rem 0 0 2rem;
   }
-  .search{
-      float:right;
-      height:30px;
-      line-height: 30px;
-      padding: 0 15px;
-      margin-right: 10px;
-      margin-top:5px;
-      background: #c1c1c1;
-      font-size: 12px;
-     
+  .search {
+    position: absolute;
+    right: 0.125rem;
+    top: 0.125rem;
+    height: 30px;
+    line-height: 30px;
+    padding: 0 15px;
+    margin: 5px 10px 0 10px;
+    background: #c1c1c1;
+    font-size: 12px;
   }
 }
 </style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <title-index></title-index>
+  </div>
+</template>
+<script>
+import TitleIndex from "../components/title/TitleIndex";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    TitleIndex
+  }
+};
+</script>
+<style scoped>
+</style>
