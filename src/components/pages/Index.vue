4b45abccb1dbbab535533d74ebd110dd623d16fc
ql <qilan@puoke.com>
2019-08-19 18:21:51 +0800

    搭建项目

---
+++ src/components/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <title-index></title-index>
+    </div>
+</template>
+<script>
+import TitleIndex from "../title/TitleIndex"
+export default {
+    data(){
+        return{
+            
+        }
+    },
+    components:{
+        TitleIndex
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/title/TitleIndex.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="titleIndex clearfix">
+            <i class="iconfont icon-ditu"></i>
+            <input type="text" placeholder="请输入查询内容">
+            <span class="search">查找</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped lang="less">
+.titleIndex{
+  height: 40px;
+  line-height: 40px;
+  background: pink;
+  display: flex;
+  i{
+      font-size: 20px;
+      padding: 0 20px 0 10px;
+      float: left;
+  }
+  input{
+      border: none;
+      border-bottom: 1px solid #cccccc;
+      padding: 8px 0;
+      background: pink;
+      width:70%;
+  }
+  .search{
+      float:right;
+      height:30px;
+      line-height: 30px;
+      padding: 0 15px;
+      margin-right: 10px;
+      margin-top:5px;
+      background: #c1c1c1;
+      font-size: 12px;
+     
+  }
+}
+</style>
